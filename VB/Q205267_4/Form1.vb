fd9289f1a09dc812995001252e0ec7a5fd72a221
DevExpressExampleBot <fakeEmail>
2023-09-10 23:52:56 +0400

    Source auto update [skip ci]

--- VB/Q205267_4/Form1.vb
+++ VB/Q205267_4/Form1.vb
@@ -1,71 +1,65 @@
-Imports Microsoft.VisualBasic
-Imports System
-Imports System.Collections.Generic
 Imports System.ComponentModel
-Imports System.Data
 Imports System.Drawing
-Imports System.Text
 Imports System.Windows.Forms
 Imports DevExpress.XtraGrid.Views.Grid
 Imports DevExpress.Xpo
 Imports Northwind
 Imports DevExpress.Data.Filtering
 Imports DevExpress.Xpo.DB
-Imports System.Threading
 
 Namespace Q205267_4
-	Partial Public Class Form1
-		Inherits Form
-		Public Sub New()
-			XpoDefault.ConnectionString = AccessConnectionProvider.GetConnectionString("..\..\nwind.mdb")
 
-			' Uncomment this code to create new database
+    Public Partial Class Form1
+        Inherits Form
 
-			'using (UnitOfWork uow = new UnitOfWork()) {
-			'    uow.ClearDatabase();
-			'    Employees employee = new Employees(uow);
-			'    employee.FirstName = "Thomas";
-			'    employee.Save();
-			'    uow.CommitChanges();
-			'    for (int i = 0; i < 10; i++) {
-			'        for (int j = 0; j < 10000; j++) {
-			'            Orders order = new Orders(uow);
-			'            order.ShipCity = "City";
-			'            order.EmployeeID = employee;
-			'        }
-			'        uow.CommitChanges();
-			'        Console.WriteLine(string.Format("{0} objects already created", i * 10000));
-			'    }
-			'}
-			InitializeComponent()
-		End Sub
+        Public Sub New()
+            XpoDefault.ConnectionString = AccessConnectionProvider.GetConnectionString("..\..\nwind.mdb")
+            ' Uncomment this code to create new database
+            'using (UnitOfWork uow = new UnitOfWork()) {
+            '    uow.ClearDatabase();
+            '    Employees employee = new Employees(uow);
+            '    employee.FirstName = "Thomas";
+            '    employee.Save();
+            '    uow.CommitChanges();
+            '    for (int i = 0; i < 10; i++) {
+            '        for (int j = 0; j < 10000; j++) {
+            '            Orders order = new Orders(uow);
+            '            order.ShipCity = "City";
+            '            order.EmployeeID = employee;
+            '        }
+            '        uow.CommitChanges();
+            '        Console.WriteLine(string.Format("{0} objects already created", i * 10000));
+            '    }
+            '}
+            InitializeComponent()
+        End Sub
 
-		Private Sub myGridView1_MasterRowGetChildList(ByVal sender As Object, ByVal e As MasterRowGetChildListEventArgs) Handles myGridView1.MasterRowGetChildList
-			Dim view As GridView = CType(sender, GridView)
-			Dim employee As Employees = CType(view.GetRow(e.RowHandle), Employees)
-			Dim source As XPServerCollectionSource = CreateChildServerModeSource(employee)
-			e.ChildList = (CType(source, IListSource)).GetList()
-		End Sub
+        Private Sub myGridView1_MasterRowGetChildList(ByVal sender As Object, ByVal e As MasterRowGetChildListEventArgs)
+            Dim view As GridView = CType(sender, GridView)
+            Dim employee As Employees = CType(view.GetRow(e.RowHandle), Employees)
+            Dim source As XPServerCollectionSource = CreateChildServerModeSource(employee)
+            e.ChildList = CType(source, IListSource).GetList()
+        End Sub
 
-		Private Function CreateChildServerModeSource(ByVal employee As Employees) As XPServerCollectionSource
-			Dim employeeId As Integer = employee.EmployeeID
-			Dim op As CriteriaOperator = New BinaryOperator("EmployeeID", employeeId)
-			Return New XPServerCollectionSource(employee.Session, GetType(Orders), op)
-		End Function
+        Private Function CreateChildServerModeSource(ByVal employee As Employees) As XPServerCollectionSource
+            Dim employeeId As Integer = employee.EmployeeID
+            Dim op As CriteriaOperator = New BinaryOperator("EmployeeID", employeeId)
+            Return New XPServerCollectionSource(employee.Session, GetType(Orders), op)
+        End Function
 
-		Private Sub myGridView1_MasterRowEmpty(ByVal sender As Object, ByVal e As MasterRowEmptyEventArgs) Handles myGridView1.MasterRowEmpty
-			Dim view As GridView = CType(sender, GridView)
-			Dim employee As Employees = CType(view.GetRow(e.RowHandle), Employees)
-			Dim source As XPServerCollectionSource = CreateChildServerModeSource(employee)
-			e.IsEmpty = (CType(source, IListSource)).GetList().Count = 0
-		End Sub
+        Private Sub myGridView1_MasterRowEmpty(ByVal sender As Object, ByVal e As MasterRowEmptyEventArgs)
+            Dim view As GridView = CType(sender, GridView)
+            Dim employee As Employees = CType(view.GetRow(e.RowHandle), Employees)
+            Dim source As XPServerCollectionSource = CreateChildServerModeSource(employee)
+            e.IsEmpty = CType(source, IListSource).GetList().Count = 0
+        End Sub
 
-		Private Sub myGridView1_MasterRowGetRelationCount(ByVal sender As Object, ByVal e As MasterRowGetRelationCountEventArgs) Handles myGridView1.MasterRowGetRelationCount
-			e.RelationCount = 1
-		End Sub
+        Private Sub myGridView1_MasterRowGetRelationCount(ByVal sender As Object, ByVal e As MasterRowGetRelationCountEventArgs)
+            e.RelationCount = 1
+        End Sub
 
-		Private Sub myGridView1_MasterRowGetRelationName(ByVal sender As Object, ByVal e As MasterRowGetRelationNameEventArgs) Handles myGridView1.MasterRowGetRelationName
-			e.RelationName = "Employee_Orders"
-		End Sub
-	End Class
+        Private Sub myGridView1_MasterRowGetRelationName(ByVal sender As Object, ByVal e As MasterRowGetRelationNameEventArgs)
+            e.RelationName = "Employee_Orders"
+        End Sub
+    End Class
 End Namespace
--- VB/Q205267_4/PersistentClasses1.vb
+++ VB/Q205267_4/PersistentClasses1.vb
@@ -1,86 +1,108 @@
-Imports Microsoft.VisualBasic
-Imports System
 Imports DevExpress.Xpo
+
 Namespace Northwind
 
-	Public Class Employees
-		Inherits XPLiteObject
-		Private fEmployeeID As Integer
-		<Key(True)> _
-		Public Property EmployeeID() As Integer
-			Get
-				Return fEmployeeID
-			End Get
-			Set(ByVal value As Integer)
-				SetPropertyValue(Of Integer)("EmployeeID", fEmployeeID, value)
-			End Set
-		End Property
-		Private fFirstName As String
-		<Size(10)> _
-		Public Property FirstName() As String
-			Get
-				Return fFirstName
-			End Get
-			Set(ByVal value As String)
-				SetPropertyValue(Of String)("FirstName", fFirstName, value)
-			End Set
-		End Property
-		<Association("Employee-Orders", GetType(Orders))> _
-		Public ReadOnly Property Orders() As XPCollection(Of Orders)
-			Get
-				Return GetCollection(Of Orders)("Orders")
-			End Get
-		End Property
-		Public Sub New(ByVal session As Session)
-			MyBase.New(session)
-		End Sub
-		Public Sub New()
-			MyBase.New(Session.DefaultSession)
-		End Sub
-		Public Overrides Sub AfterConstruction()
-			MyBase.AfterConstruction()
-		End Sub
-	End Class
-	Public Class Orders
-		Inherits XPLiteObject
-		Private fOrderID As Integer
-		<Key(True)> _
-		Public Property OrderID() As Integer
-			Get
-				Return fOrderID
-			End Get
-			Set(ByVal value As Integer)
-				SetPropertyValue(Of Integer)("OrderID", fOrderID, value)
-			End Set
-		End Property
-		Private fEmployeeID As Employees
-		<Association("Employee-Orders")> _
-		Public Property EmployeeID() As Employees
-			Get
-				Return fEmployeeID
-			End Get
-			Set(ByVal value As Employees)
-				SetPropertyValue(Of Employees)("EmployeeID", fEmployeeID, value)
-			End Set
-		End Property
-		Private fShipCity As String
-		<Size(15)> _
-		Public Property ShipCity() As String
-			Get
-				Return fShipCity
-			End Get
-			Set(ByVal value As String)
-				SetPropertyValue(Of String)("ShipCity", fShipCity, value)
-			End Set
-		End Property
-		Public Sub New(ByVal session As Session)
-			MyBase.New(session)
-		End Sub
-		Public Sub New()
-			MyBase.New(Session.DefaultSession)
-		End Sub
-		Public Overrides Sub AfterConstruction()
-			MyBase.AfterConstruction()
-		End Sub
-	End Class
+    Public Class Employees
+        Inherits XPLiteObject
+
+        Private fEmployeeID As Integer
+
+        <Key(True)>
+        Public Property EmployeeID As Integer
+            Get
+                Return fEmployeeID
+            End Get
+
+            Set(ByVal value As Integer)
+                SetPropertyValue(Of Integer)("EmployeeID", fEmployeeID, value)
+            End Set
+        End Property
+
+        Private fFirstName As String
+
+        <Size(10)>
+        Public Property FirstName As String
+            Get
+                Return fFirstName
+            End Get
+
+            Set(ByVal value As String)
+                SetPropertyValue("FirstName", fFirstName, value)
+            End Set
+        End Property
+
+        <Association("Employee-Orders", GetType(Orders))>
+        Public ReadOnly Property Orders As XPCollection(Of Orders)
+            Get
+                Return GetCollection(Of Orders)("Orders")
+            End Get
+        End Property
+
+        Public Sub New(ByVal session As Session)
+            MyBase.New(session)
+        End Sub
+
+        Public Sub New()
+            MyBase.New(Session.DefaultSession)
+        End Sub
+
+        Public Overrides Sub AfterConstruction()
+            MyBase.AfterConstruction()
+        End Sub
+    End Class
+
+    Public Class Orders
+        Inherits XPLiteObject
+
+        Private fOrderID As Integer
+
+        <Key(True)>
+        Public Property OrderID As Integer
+            Get
+                Return fOrderID
+            End Get
+
+            Set(ByVal value As Integer)
+                SetPropertyValue(Of Integer)("OrderID", fOrderID, value)
+            End Set
+        End Property
+
+        Private fEmployeeID As Employees
+
+        <Association("Employee-Orders")>
+        Public Property EmployeeID As Employees
+            Get
+                Return fEmployeeID
+            End Get
+
+            Set(ByVal value As Employees)
+                SetPropertyValue("EmployeeID", fEmployeeID, value)
+            End Set
+        End Property
+
+        Private fShipCity As String
+
+        <Size(15)>
+        Public Property ShipCity As String
+            Get
+                Return fShipCity
+            End Get
+
+            Set(ByVal value As String)
+                SetPropertyValue("ShipCity", fShipCity, value)
+            End Set
+        End Property
+
+        Public Sub New(ByVal session As Session)
+            MyBase.New(session)
+        End Sub
+
+        Public Sub New()
+            MyBase.New(Session.DefaultSession)
+        End Sub
+
+        Public Overrides Sub AfterConstruction()
+            MyBase.AfterConstruction()
+        End Sub
+    End Class
 End Namespace
